71b2019c601357837dd4233add8fefcc9b303321
Song Yang <songyang1218@gmail.com>
2020-09-12 17:25:51 -0400

    Count script that logs time and word counts to files

---
+++ count.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Usage
+# Input a directory path as an argument.
+# Everything inside the directory is counted.
+# Output is redirected to two log files.
+
+# Instructions
+if [ "$#" != 1 ]; then
+	echo "Usage: ./count.sh path-to-a-directory"
+	exit 0
+fi
+
+# file logs
+TIME_OUTPUT='./time.log'
+COUNT_OUTPUT='./count.log'
+
+# initialize empty logs
+if test -f "$TIME_OUTPUT"; then
+	cat /dev/null > "$TIME_OUTPUT"
+else
+	touch "$TIME_OUTPUT"
+fi
+
+if test -f "$COUNT_OUTPUT"; then
+	cat /dev/null > "$COUNT_OUTPUT"
+else
+	touch "$COUNT_OUTPUT"
+fi
+
+# loop through every file in the directory
+for file in $1/*
+do
+	if [ -d "$file" ]; then
+		continue
+	fi
+	/usr/bin/time -o "$TIME_OUTPUT" --append --format='%E' \
+	       	cat $file | tr ' ' '\n' | wc -w \
+		>> "$COUNT_OUTPUT"
+done
